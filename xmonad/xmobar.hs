1e179454409cb8aeb1c2544345327beb0991db00
Daniel Hines <d4hines@gmail.com>
2021-09-18 13:08:33 -0400

    fixed xmobar

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -6,7 +6,6 @@ main :: IO ()
 main = do
   xmobar config
 
-config :: Config
 config =
   defaultConfig
     { font = "xft:Fira Code:size=14:antialias=true:hinting=true:bold,Noto Color Emoji:size=14:antialias=true:hinting=true",
@@ -15,15 +14,14 @@ config =
       position = BottomSize C 100 24,
       sepChar = "%", -- delineator between plugin names and straight text
       alignSep = "}{", -- separator between left-right alignment
-      template = " %cpu% | %memory% } %complice% { %date% | %time_norfolk% | %time_paris%",
+      template = " %cpu% | %memory% } %StdinReader% { %date% | %time_norfolk% Norfolk | %time_paris% Paris | %time_india% Calcutta",
       commands =
-        [ Run $ Date "%a, %d %b %Y" "date" 10,
-          Run $ Date "%I:%M %p" "time_norfolk" 10,
-          Run $ Cpu ["-L", "3", "-H", "50", "--normal", "green", "--high", "red"] 10,
-          Run $ Memory ["-t", "Mem: <usedratio>%"] 10,
-          -- TODO: replace with direct haskell code
-          Run $ Com "paris_date" ["+%I:%M %p"] "time_paris" 10,
-          -- TODO: replace with direct haskell code
-          Run $ Com "complice" [] "complice" 30
+        [ Run $ Date "%a, %d %b %Y" "date" 10
+          ,Run $ Cpu ["-L", "3", "-H", "50", "--normal", "green", "--high", "red"] 10
+          ,Run $ Memory ["-t", "Mem: <usedratio>%"] 10
+          ,Run $ StdinReader
+          ,Run $ DateZone "%I:%M %p" "en_US.UTF-8" "America/New_York" "time_norfolk" 10
+          ,Run $ DateZone "%I:%M %p" "en_US.UTF-8" "Europe/Paris" "time_paris" 10
+          ,Run $ DateZone "%I:%M %p" "en_US.UTF-8" "Asia/Calcutta" "time_india" 10
         ]
     }
