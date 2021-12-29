89946fe306008ea2df1b2da4d16764a4b6622145
Daniel Hines <d4hines@gmail.com>
2021-12-29 07:14:10 -0500

    xmonad works again!

--- overlays/xmonad.nix
+++ overlays/xmonad.nix
@@ -4,21 +4,22 @@ final: prev: rec {
       overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
         (
           self: super: rec {
-            xmobar = super.xmobar.overrideAttrs (
-              old: {
-                configureFlags = old.configureFlags ++ [
-                  "-fwith_datezone"
-                ];
-              }
-            );
-            my-xmonad = self.callCabal2nix "my-xmonad"
-              (
-                prev.lib.sourceByRegex ../xmonad [
-                  "xmonad.hs"
-                  "xmobar.hs"
-                  "my-xmonad.cabal"
-                ]
-              ) {};
+            # xmobar = super.xmobar.overrideAttrs (
+            #   old: {
+            #     configureFlags = old.configureFlags ++ [
+            #       "-fwith_datezone"
+            #     ];
+            #   }
+            # );
+            # xmonad = self.callCabal2nix "my-xmonad"
+            #   (
+            #     prev.lib.sourceByRegex ../xmonad [
+            #       "xmonad.hs"
+            #       "xmobar.hs"
+            #       "my-xmonad.cabal"
+            #     ]
+            #   ) {};
+            xmonad = self.xmonad_0_17_0;
           }
         );
     }
