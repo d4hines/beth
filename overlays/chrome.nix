4a34d9cdba3b1a811af835bef2e9e459f273c1fe
Daniel Hines <d4hines@gmail.com>
2022-01-05 11:30:12 -0500

    consolidate overlays

--- overlays/chrome.nix
+++ overlays/chrome.nix
@@ -3,4 +3,8 @@ final: prev: with prev; {
     ''#!/usr/bin/env bash
       ${prev.google-chrome}/bin/google-chrome-stable --remote-debugging-port=9222 "$@"
     '';
+  brave = prev.writeScriptBin "brave"
+    ''#!/usr/bin/env bash
+      ${prev.brave}/bin/brave --remote-debugging-port=9222 "$@"
+    '';
 }
