459eef79b254fee49c6ddcaa584ec7e13c6bb4b5
Daniel Hines <d4hines@gmail.com>
2022-01-05 11:30:12 -0500

    consolidate overlays

--- overlays/brave.nix
+++
@@ -1,6 +0,0 @@
-final: prev: with prev; {
-  brave = prev.writeScriptBin "brave"
-    ''#!/usr/bin/env bash
-      ${prev.brave}/bin/brave --remote-debugging-port=9222 "$@"
-    '';
-}
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,4 @@
 [
-  (import ./brave.nix)
   (import ./chrome.nix)
   (import ./scripts.nix)
   (import ./haskell-stuff.nix)
