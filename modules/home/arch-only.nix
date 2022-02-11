aa3108d3844ff9e8c42a993fe8798e492e6c5d7d
Daniel Hines <d4hines@gmail.com>
2022-02-11 14:34:42 -0500

    fix vs code keyring issue

--- modules/home/arch-only.nix
+++ modules/home/arch-only.nix
@@ -7,6 +7,12 @@
   home.file.".xinitrc" = {
     text = ''
       [ -f ~/.xprofile ] && . ~/.xprofile
+
+      # needed for VS Code to store secrets correctly
+      source /etc/X11/xinit/xinitrc.d/50-systemd-user.sh
+      eval $(gnome-keyring-daemon --start)
+      export SSH_AUTH_SOCK
+
       systemctl --user start browser-whitelist
       systemctl --user start twitch-notifications
       systemctl --user start complice-xmobar
