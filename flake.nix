3ba1051184e42f82de7aaa872ac20415ecb49c1c
Daniel Hines <d4hines@gmail.com>
2022-04-08 11:08:23 -0400

    remove containerd shim taht doesn't work

--- flake.nix
+++ flake.nix
@@ -29,7 +29,6 @@
             overlay-module
             ./modules/configuration.nix
             ./modules/sound.nix
-            ./modules/containerd-shim-hack.nix
             ./modules/cron.nix
             home.nixosModules.home-manager
             {
