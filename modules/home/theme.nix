2117324e53e8a5376c1170f95ef10d3fc3724d89
Daniel Hines <d4hines@gmail.com>
2022-12-17 11:10:05 -0500

    change some colors

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -1,13 +1,13 @@
 # Based on https://github.com/azemoh/vscode-one-monokai with thanks
 {
-  PLAIN_COLOR = "#abb2bf";
+  PLAIN_COLOR = "#C6D0F5";
   GREY_COLOR = "#51576D";
-  DARK_GREY_COLOR = "#303446";
+  DARK_GREY_COLOR = "#232634";
   CARET_COLOR = "#528bff";
   PINK_COLOR = "#e06c75";
   CYAN_COLOR = "#56b6c2";
   LIME_COLOR = "#98c379";
-  PURPLE_COLOR = "#c678dd";
+  PURPLE_COLOR = "#CA9EE6";
   BROWN_COLOR = "#d19a66";
   GOLD_COLOR = "#e5c07b";
   BLUE_COLOR = "#8CAAEE";
