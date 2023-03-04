a56d7354ff9c14344c471b785aed2f80dce64151
Daniel Hines <d4hines@gmail.com>
2023-03-04 10:03:52 -0500

    unblock twitter host since I'm using freedom

--- machines/RADAH/default.nix
+++ machines/RADAH/default.nix
@@ -59,9 +59,6 @@
       networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
       networking.wireless.networks."${builtins.readFile ../../secrets/ssid}".psk = builtins.readFile ../../secrets/wifi_psk;
       networking.wireless.userControlled.enable = true;
-      networking.extraHosts = ''
-        127.0.0.1 twitter.com
-      '';
 
       time.timeZone = "America/New_York";
 
