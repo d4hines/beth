57e64cd55b06160c597baad5c7a59a714c41135f
Daniel Hines <d4hines@gmail.com>
2023-09-14 11:09:09 -0400

    homebrew stuff

--- machines/DARESH/default.nix
+++ machines/DARESH/default.nix
@@ -17,6 +17,8 @@
         export GPG_TTY="$(tty)"
         export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
         gpgconf --launch gpg-agent
+
+        eval "$(/opt/homebrew/bin/brew shellenv)"
       '';
     };
   }
