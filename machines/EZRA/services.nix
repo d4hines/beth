1c31acc755f8d56b21b0d05fa502134160f2ed14
Daniel Hines <d4hines@gmail.com>
2024-02-08 12:52:58 -0500

    eds survey stuff

--- machines/EZRA/services.nix
+++ machines/EZRA/services.nix
@@ -1,22 +1,26 @@
-{
-  pkgs,
-  config,
-  ...
+{ pkgs
+, config
+, ...
 }: {
   age.secrets.ezra-token.file = ../../secrets/ezra-token.age;
   age.secrets.eds-survey-api-token.file = ../../secrets/eds-survey-api-token.age;
-  users.groups.cloudflared = {};
+  users.groups.cloudflared = { };
   users.users.cloudflared = {
     isSystemUser = true;
     hashedPassword = "*";
     group = "cloudflared";
   };
+  users.groups.edssurvey = { };
+  users.users.edssurvey = {
+    isSystemUser = true;
+    hashedPassword = "*";
+    group = "edssurvey";
+  };
   systemd.services.ssh-tunnel = {
     description = "SSH Tunnel";
-    environment = {
-    };
-    wantedBy = ["multi-user.target"];
-    after = ["network.target"]; # if networking is needed
+    environment = { };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ]; # if networking is needed
 
     restartIfChanged = true; # set to false, if restarting is problematic
 
@@ -26,7 +30,7 @@
       Restart = "on-failure";
       User = "cloudflared";
       Group = "cloudflared";
-      ReadWritePaths = [];
+      ReadWritePaths = [ ];
       PrivateTmp = "true";
       ProtectSystem = "full";
       NoNewPrivileges = "true";
@@ -34,10 +38,9 @@
   };
   systemd.services.eds-survey-api-tunnel = {
     description = "EDS Survey API Tunnel";
-    environment = {
-    };
-    wantedBy = ["multi-user.target"];
-    after = ["network.target"]; # if networking is needed
+    environment = { };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ]; # if networking is needed
 
     restartIfChanged = true; # set to false, if restarting is problematic
 
@@ -47,15 +50,36 @@
       Restart = "on-failure";
       User = "cloudflared";
       Group = "cloudflared";
-      ReadWritePaths = [];
+      ReadWritePaths = [ ];
       PrivateTmp = "true";
       ProtectSystem = "full";
       NoNewPrivileges = "true";
     };
   };
+  systemd.services.eds-survey-api = {
+    description = "EDS Survey API";
+    environment = {
+      PATH_TO_SURVEY = "/home/d4hines/OneDrive/eds_data.xlsx";
+    };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ]; # if networking is needed
+
+    restartIfChanged = true; # set to false, if restarting is problematic
+    serviceConfig = {
+      ExecStart = "${pkgs.bun}/bin/bun /home/d4hines/eds_survey/index.ts";
+      Restart = "on-failure";
+      User = "edssurvey";
+      Group = "edssurvey";
+      ReadWritePaths = [ ];
+      PrivateTmp = "true";
+      ProtectSystem = "full";
+      NoNewPrivileges = "true";
+    };
+
+  };
   virtualisation.oci-containers = {
     containers.homeassistant = {
-      volumes = ["/home_assistant_config:/config"];
+      volumes = [ "/home_assistant_config:/config" ];
       environment.TZ = "America/New_York";
       image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [
@@ -78,6 +102,6 @@
   };
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [1883];
+    allowedTCPPorts = [ 1883 ];
   };
 }
