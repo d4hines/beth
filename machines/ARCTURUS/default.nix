fe26810a53eb2951c3dd2e1d00d08e739ffaccde
Daniel Hines <d4hines@gmail.com>
2024-10-29 12:17:22 -0400

    wip

--- machines/ARCTURUS/default.nix
+++ machines/ARCTURUS/default.nix
@@ -27,17 +27,39 @@
             experimental-features = nix-command flakes
           '';
         };
+        nixpkgs.config.allowUnfree = true;
         networking.hostName = "ARCTURUS";
+        networking.networkmanager.enable = true;
+        networking.networkmanager.wifi.powersave = false;
+        # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+        # Per-interface useDHCP will be mandatory in the future, so this generated config
+        # replicates the default behaviour.
+        networking.useDHCP = false;
+
+        networking.nameservers = ["1.1.1.1" "9.9.9.9"];
         time.timeZone = "America/New_York";
         environment.systemPackages = with pkgs; [
           vim
           htop
           git
           toolbox
+          efibootmgr
+          gnome3.adwaita-icon-theme
+          mednafen
+          mednaffe
+          superTuxKart
+          chromium
+          haskellPackages.play-xmonad
 
           nodejs
           yarn
         ];
+        programs.nix-ld.enable = true;
+        programs.nix-ld.libraries = with pkgs; [
+          # Add any missing dynamic libraries for unpackaged programs
+          # here, NOT in environment.systemPackages
+        ];
+
         environment.etc."revision".text = "${rev}";
 
         services.openssh = {
@@ -45,11 +67,15 @@
           settings.PasswordAuthentication = false;
         };
 
+        services.xserver.enable = true;
+        services.xserver.dpi = 96;
+        services.xserver.displayManager.startx.enable = true;
+
         users = {
           mutableUsers = false;
           users."d4hines" = {
             isNormalUser = true;
-            extraGroups = ["wheel"];
+            extraGroups = ["wheel" "networkmanager" "video"];
             openssh.authorizedKeys.keyFiles = [../../keys/authorized_keys];
           };
         };
